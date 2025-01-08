9b9c3ce9dc4e223cb2ee8e1b46ed0895c8b2e0ef
chenzhuo <178320938@qq.com>
2025-01-08 12:40:18 +0800

    演示环境

--- app/Http/Middleware/BadSqlGrammar.php
+++ app/Http/Middleware/BadSqlGrammar.php
@@ -9,9 +9,18 @@ use Illuminate\Http\Request;
 
 class BadSqlGrammar
 {
+    // 忽略列表
+    protected $except = [
+        'admin/system/auth/login',
+        'admin/system/auth/logout',
+        'admin/system/auth/refresh-token',
+    ];
 
     public function handle(Request $request, Closure $next)
     {
+        if ($this->inExceptArray($request)) {
+            return $next($request);
+        }
         if ($this->isDevEnvironment()) {
             if (in_array($request->method(), ['POST', 'PUT', 'DELETE'])) {
                 // 处理 POST PUT DELETE请求
@@ -24,5 +33,18 @@ class BadSqlGrammar
     protected function isDevEnvironment()
     {
         return app()->environment('dev');
+    }
+
+    protected function inExceptArray($request)
+    {
+        foreach ($this->except as $except) {
+            if ($except !== '/') {
+                $except = trim($except, '/');
+            }
+            if ($request->fullUrlIs($except) || $request->is($except)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
