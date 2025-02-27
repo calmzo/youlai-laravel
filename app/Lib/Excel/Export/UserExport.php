38a3468133469edef3cd9c37609c609ceced570a
chenzhuo <178320938@qq.com>
2025-02-27 14:34:33 +0800

    用户模板导出

--- app/Lib/Excel/Export/UserExport.php
+++ app/Lib/Excel/Export/UserExport.php
@@ -235,7 +235,7 @@ class UserExport extends BaseExport implements BaseExportInterface
             '未知'
         ];
 
-        $validation = $objSheet->getCell('C2')->getDataValidation();
+        $validation = $objSheet->getCell('C3')->getDataValidation();
         $validation->setType(DataValidation::TYPE_LIST);
         $validation->setFormula1(implode(',', $rules)); // 设置下拉列表的选项
         $validation->setShowDropDown(TRUE); // 显示下拉箭头
